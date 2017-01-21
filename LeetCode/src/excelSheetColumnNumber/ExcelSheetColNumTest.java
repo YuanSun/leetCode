3e33bccd9baf29acfb044d340e14920cdf521456
YuanSun <reggie.sunyuan@me.com>
2017-01-21 09:40:23 -0500

    ExcelColNumber

---
+++ LeetCode/src/excelSheetColumnNumber/ExcelSheetColNumTest.java
@@ -0,0 +1,14 @@
+package excelSheetColumnNumber;
+
+public class ExcelSheetColNumTest {
+
+	public static void main(String[] args) {
+		String Col = "aca";
+		
+		double ColNum = excelSheetColumnNumber.Solution.ExcelColNum(Col);
+		
+		System.out.println("The column number is " + ColNum);
+
+	}
+
+}
---
+++ LeetCode/src/excelSheetColumnNumber/Solution.java
@@ -0,0 +1,13 @@
+package excelSheetColumnNumber;
+
+public class Solution {
+	public static double ExcelColNum(String Col) {
+		Col = Col.toUpperCase();
+		double num = 0;
+		int length = Col.length(); 
+		for (int i = 0; i < length; i++)
+			num = (num + (Col.charAt(length - i - 1) - 'A' + 1) * Math.pow(26, i));
+		
+		return num;
+	}
+}
